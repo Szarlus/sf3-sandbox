4e02b6a1ee231a3926cc6cbc5ef16a16fc3079dc
pawelbaranski <pawel.baranski1@gmail.com>
2017-03-22 23:17:05 +0000

    voter tests

---
+++ tests/unit/AppBundle/Security/Voter/tests/unit/AppBundle/Security/Voter/CanChangeTaskStatusVoterTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace tests\unit\AppBundle\Security\Voter;
+
+
+use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
+use AppBundle\Security\Voter\CanChangeTaskStatusVoter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class CanChangeTaskStatusVoterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var CanChangeTaskStatusVoter */
+    private $voter;
+
+    /** @var TokenInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $token;
+
+    protected function setUp()
+    {
+        $this->token = $this->tokenMock();
+        $this->voter = new CanChangeTaskStatusVoter();
+    }
+
+    /** @test */
+    public function itAbstainsFromVotingIfSubjectIsNotATask()
+    {
+        $this->assertSame(Voter::ACCESS_ABSTAIN, $this->voter->vote(
+            $this->token,
+            new \stdClass(),
+            ['change_status']
+        ));
+    }
+
+    /** @test */
+    public function itAbstainsFromVotingIfAttributeIsNotStatusChange()
+    {
+        $this->assertSame(Voter::ACCESS_ABSTAIN, $this->voter->vote(
+            $this->token,
+            new Task(),
+            ['something']
+        ));
+    }
+
+    /** @test */
+    public function itDeniesAccessIfThereIsNoUserToVoteFor()
+    {
+        $this->tokenWithoutUser();
+
+        $this->assertSame(Voter::ACCESS_DENIED, $this->voter->vote(
+            $this->token,
+            new Task(),
+            ['change_status']
+        ));
+    }
+
+    /** @test */
+    public function itDeniesAccessIfUserIsNotAssignedToTheTask()
+    {
+        $task = new Task();
+        $this->tokenReturns(new User());
+
+        $this->assertSame(Voter::ACCESS_DENIED, $this->voter->vote(
+            $this->token,
+            $task,
+            ['change_status']
+        ));
+    }
+
+    /** @test */
+    public function itGrantsAccessIfUserIsAssignedToTheTask()
+    {
+        $user = new User();
+        $task = new Task();
+        $task->setAssignedTo($user);
+        $this->tokenReturns($user);
+
+        $this->assertSame(Voter::ACCESS_GRANTED, $this->voter->vote(
+            $this->token,
+            $task,
+            ['change_status']
+        ));
+    }
+
+    private function tokenMock()
+    {
+        return $this
+            ->getMockBuilder(TokenInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    private function tokenReturns($user)
+    {
+        $this->token
+            ->expects($this->any())
+            ->method('getUser')
+            ->willReturn($user);
+    }
+
+    private function tokenWithoutUser()
+    {
+        $this->tokenReturns(null);
+    }
+}
